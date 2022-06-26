70870025a3ef9d0756f669b1f66e83ec300af3f3
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-06-26 12:38:45 -0500

    Rating survey styles done

--- src/components/RatingSurvey.vue
+++ src/components/RatingSurvey.vue
@@ -1,7 +1,22 @@
 <template>
-  <p>Hello</p>
-  <p>Hello</p>
-  <p>Hello</p>
+  <section class="survey-container">
+    <div class="star-icon-container">
+      <img
+        class="star-icon"
+        src="@/assets/images/icon-star.svg"
+        alt="Star Icon"
+      />
+    </div>
+    <h1 class="survey-title">How did we do?</h1>
+    <p class="survey-description">
+      Please let us know how we did with your support request. All feedback is
+      appreciated to help us improve our offering!
+    </p>
+    <div class="buttons-container">
+      <button class="rating-button">1</button>
+    </div>
+    <button class="submit-button">SUBMIT</button>
+  </section>
 </template>
 
 <script>
@@ -12,7 +27,68 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-p {
-  color: red;
+.survey-container {
+  --color-white: hsl(0, 0%, 100%);
+  --color-light-grey: hsl(217, 12%, 63%);
+  --color-medium-grey: hsl(218, 12%, 32%);
+  --color-dark-blue: hsl(213, 19%, 18%);
+  --color-very-dark-blue: hsl(216, 12%, 8%);
+  --color-orange: hsl(25, 97%, 53%);
+  background-color: var(--color-dark-blue);
+  border-radius: 20px;
+  padding: 2em;
+  margin: 1.5em;
+  color: var(--color-white);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.star-icon-container {
+  background-color: var(--color-medium-grey);
+  border-radius: 50%;
+  height: 35px;
+  width: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1.5em;
+}
+.survey-title {
+  margin: 0;
+}
+.survey-description {
+  text-align: left;
+  color: var(--color-light-grey);
+  margin-bottom: 0;
+  margin-top: 1em;
+}
+
+.buttons-container {
+  display: flex;
+  justify-content: space-between;
+  margin: 1.5em 0;
+}
+
+.rating-button {
+  background-color: var(--color-medium-grey);
+  border-radius: 50%;
+  border: none;
+  height: 35px;
+  width: 35px;
+  color: var(--color-light-grey);
+}
+
+.rating-button:hover {
+  background-color: var(--color-orange);
+  color: var(--color-white);
+}
+
+.submit-button {
+  background-color: var(--color-orange);
+  border-radius: 20px;
+  border: none;
+  width: 100%;
+  height: 35px;
+  color: var(--color-white);
 }
 </style>
